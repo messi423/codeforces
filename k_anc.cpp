8277fd68917ffa9735e03fde3527b5f71c111f94
KARAN SINGH <karannarendrasi@gmail.com>
2021-07-06 23:03:49 +0700

    added k_lca

--- k_anc.cpp
+++ k_anc.cpp
@@ -2,10 +2,12 @@ struct k_anc{
     int n;
     vector<int>par;
     vector<vector<int>>dp;
+    bool done;
 
     inline void init(int n1){
         n=n1;
         par.resize(n);
+        done=0;
         dp = vector<vector<int>>(n,vector<int>(log2(n)+1, -1));
     }
 
@@ -20,9 +22,14 @@ struct k_anc{
                 else dp[i][l]=-1;
             }
         }
+        done=1;
     }
 
     inline int query(int s, int k){
+        if(!done){
+            cerr << "Call preprocess first you dope!!" << "\n";
+            exit(0);
+        }
         int v=s;
         while(k>0 && v!=-1){
             int up = log2(k&(-k));
